61162398b0bd7a5ddbc36f1c79b4bf42c66a3483
wasadmin <wasadmin@example.com>
2024-07-02 16:08:25 -0400

    server

--- server/routers/filmsRouter.js
+++ server/routers/filmsRouter.js
@@ -1,4 +1,30 @@
 import express from 'express';
 const filmsRouter = express.Router();
 
+filmsRouter.get('/', (req, res) => {
+    res.send('wassup')
+})
+filmsRouter.get('/:id', (req, res) => {
+    const { id } = req.params
+    res.send('please tell me its working')
+})
+
+filmsRouter.get('/:id/', (req, res) => {
+    const { id } = req.params
+    res.send('tell me its working')
+        
+})
+
+filmsRouter.get('/:id/characters', (req, res) => {
+    const { id } = req.params
+    res.send('please tell me its working')
+
+})
+
+filmsRouter.get('/:id/planets', (req, res) => {
+    const { id } = req.params
+    res.send('tell me its working')
+
+})
+
 export default filmsRouter;
