43dd8b0ef30c613914df1f9fb220ab75b1d116a3
wasadmin <wasadmin@example.com>
2024-07-03 11:02:00 -0400

    router changes

--- server/routers/filmsRouter.js
+++ server/routers/filmsRouter.js
@@ -1,4 +1,14 @@
 import express from 'express';
+import { MongoClient } from "mongodb";
+ 
+const url = 'mongodb://localhost:27017'
+const filmDB = 'swapi';
+const filmCollect = 'films'
+
+const client = await MongoClient.connect(url);
+const db = client.db(filmDB);
+const collection = db.collection(filmCollect);
+
 const filmsRouter = express.Router();
 
 filmsRouter.get('/', (req, res) => {
@@ -26,5 +36,15 @@ filmsRouter.get('/:id/planets', (req, res) => {
     res.send('tell me its working')
 
 })
+
+filmsRouter.get("/", async (req, res) => {
+    try {
+      const film = await collection.find({}).toArray();
+      res.json(film);
+    } catch (err) {
+      console.error("Error:", err);
+      res.status(500).send("Hmmm, something's wrong... No films")
+    }
+  });
 
 export default filmsRouter;
--- server/routers/planetsRouter.js
+++ server/routers/planetsRouter.js
@@ -1,4 +1,14 @@
 import express from 'express';
+import { MongoClient } from "mongodb";
+
+const url = 'mongodb://localhost:27017'
+const planetDB = 'swapi';
+const planetCollect = 'planets'
+
+const client = await MongoClient.connect(url);
+const db = client.db(planetDB);
+const collection = db.collection(planetCollect);
+
 const planetsRouter = express.Router();
 
 planetsRouter.get('/', (req, res) => {
@@ -22,5 +32,15 @@ planetsRouter.get('/:id/characters', (req, res) => {
     res.send('please tell me its working')
 
 })
+
+planetsRouter.get("/", async (req, res) => {
+    try {
+      const planet = await collection.find({}).toArray();
+      res.json(planet);
+    } catch (err) {
+      console.error("Error:", err);
+      res.status(500).send("Hmmm, something's wrong... No planets for you! ☹");
+    }
+  });
 
 export default planetsRouter;
